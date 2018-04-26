e9b9540950b9b49974fa15ec163d132470305ea0
shawn <shawn.cui@cmdspace.cn>
2018-04-26 17:06:28 +0800

    feat: import necessary modules/styles and add interceptors for http requests

--- src/main.js
+++ src/main.js
@@ -1,9 +1,34 @@
+import 'element-ui/lib/theme-chalk/index.css'
+import 'element-ui/lib/theme-chalk/display.css'
+// fade/zoom 等
+import 'element-ui/lib/theme-chalk/base.css'
+
 import Vue from 'vue'
+import ElementUI from 'element-ui'
+import axios from 'axios'
+
+import router from './router'
 import App from './App'
+
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  router,
+  template: '<App/>',
+  components: { App },
+  created() {
+    const url = process.env.API_ROOT
+    axios.interceptors.request.use(function (config) {
+      if (config.url.indexOf(url) === -1) {
+        config.url = url + config.url
+      }
+      return config;
+    }, function (error) {
+      return Promise.reject(error)
+    })
+  }
 })
