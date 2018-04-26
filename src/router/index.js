e55e046404da35ba7a59a9823cce884166065e06
shawn <shawn.cui@cmdspace.cn>
2018-04-26 17:02:51 +0800

    feat: create router/index.js

---
+++ src/router/index.js
@@ -0,0 +1,35 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Layout from 'views/layout/Layout'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import('@/views/login/Login')
+    },
+    {
+      path: '/',
+      component: Layout,
+      children: [
+        {
+          path: '/',
+          name: 'news',
+          component: () => import('@/views/news/News')
+        },
+        {
+          path: '/news/edit',
+          name: 'news.edit',
+          component: () => import('@/views/news/Edit')
+        }
+      ]
+    },
+    {
+      path: '*',
+      redirect: '/'
+    }
+  ]
+})
